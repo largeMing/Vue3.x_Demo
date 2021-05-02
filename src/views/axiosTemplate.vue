6b280d747fe51c1defd3c2a304c98f868d7ff12f
Aming <814067990@qq.com>
2021-05-02 14:08:40 +0800

    init

---
+++ src/views/axiosTemplate.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import axios from "../utils/axios";
+
+export default defineComponent ({
+    setup(){
+        axios.get('/user/XPoet').then((res) => {
+            console.log('res: ', res);
+        }).catch((err) => {
+            console.log('err: ', err);
+        })
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
