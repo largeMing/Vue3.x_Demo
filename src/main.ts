2d67cf565fcdacd63da06b94d73a1510c2c02626
Aming <814067990@qq.com>
2021-05-03 12:28:59 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+// eslint-disable-next-line no-unused-vars
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
+// eslint-disable-next-line no-unused-vars,import/extensions,import/no-unresolved
 import router from './router'
+// eslint-disable-next-line no-unused-vars,import/no-unresolved,import/extensions
 import store from './store'
-import ElementPlus from 'element-plus'
+// eslint-disable-next-line no-unused-vars
 import 'element-plus/lib/theme-chalk/index.css'
 
 createApp(App).mount('#app')
